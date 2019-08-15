c08485179790ea496562cab4b188a98ac2e505ae
Samuel <raulsamuelleos98@gmail.com>
2019-08-15 12:50:53 -0500

    Updates

---
+++ precache-manifest.2b8975fd3336a54d8b029239ceb0ce51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31494c91b91f155579d5b1d652a0636c",
+    "url": "/branlo-web2/index.html"
+  },
+  {
+    "revision": "9715752e9c58661cc0d7",
+    "url": "/branlo-web2/static/css/main.05f0c305.chunk.css"
+  },
+  {
+    "revision": "b2e5abbda2d9395c0ab2",
+    "url": "/branlo-web2/static/js/2.624dd4e9.chunk.js"
+  },
+  {
+    "revision": "9715752e9c58661cc0d7",
+    "url": "/branlo-web2/static/js/main.ce4df597.chunk.js"
+  },
+  {
+    "revision": "2e70455bce3509f124bf",
+    "url": "/branlo-web2/static/js/runtime~main.c3221645.js"
+  },
+  {
+    "revision": "a73a65ab869d7d789beb71cde7ba2448",
+    "url": "/branlo-web2/static/media/fondoBranlo.a73a65ab.jpg"
+  }
+]);
