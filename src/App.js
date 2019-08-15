cf79a0e698b0dd821e7e8cdb3739e80f2e426295
chsDevs <chs.desarolladores@gmail.com>
2019-08-14 21:27:59 -0500

    Quienes y Acerca de nosotros agregados, falta diseno de links y quienes somos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navegacion from './container/Navegacion/Navegacion';
+import QuienesSomos from './container/QuienesSomos/QuienesSomos';
+import AcercaDeNosotros from './container/AcercaDeNosotros/AcercaDeNosotros';
 
 function App() {
   return (
     <Router>
       <Navegacion/>
+      <Route exact path="/" component={QuienesSomos} />
+      <Route path="/AcercaDeNosotros" component={AcercaDeNosotros}/>
     </Router>
   );
 }
---
+++ src/container/QuienesSomos/QuienesSomos.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class QuienesSomos extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-sm-12" align="center">
+                        <h1>QUIENES SOMOS</h1>
+                    </div>
+                </div>
+                <div className="container mt-3">
+                    <div className="col-sm-12">
+                        <p align="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, optio vitae magnam commodi laudantium aliquid accusantium impedit modi vero minus omnis sunt enim, at distinctio quae minima ullam? Eveniet, quo!</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
